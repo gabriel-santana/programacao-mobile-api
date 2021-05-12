f17f98f0cfb4a517e82d5df42d6ed7fba79c35d8
luanhenrique02 <luanhenrique092010@hotmail.com>
2021-05-12 16:43:17 -0300

    valor banco de dados

--- createdb.sql
+++ createdb.sql
@@ -26,7 +26,6 @@ INSERT INTO ong
 (nome, `desc`, imagem, site, cnpj, pix)
 VALUES('Criança Feliz', 'Nossa ONG tem o obetivo de fazer as crianças felizes, através de sua doação!', '/storage/crianca_feliz.jpg', 'https://redeglobo.globo.com/rpc/', '99.999.999/0001-99', '99.999.999/0001-99');
 
-
 INSERT INTO ong
 (nome, `desc`, imagem, site, cnpj, pix)
 VALUES('Lar de Idosos Santa Mônica', 'Nossa ONG tem o obetivo de fazer as idosos felizes, através de sua doação!', '/storage/lar_de_idosos_santa_monica.png', 'https://www.spacex.com/', '99.999.999/0001-99', '99.999.999/0001-99');
@@ -45,3 +44,7 @@ VALUES(1, 'Marcelo Chamuska', 'marcelo.cham@hotmail.com', '(41)99999-9999');
 INSERT INTO contato
 (id_ong, nome, email, telefone)
 VALUES(2, 'Ana Maria Monica', 'maria.monica@hotmail.com', '(41)99999-9999');
+
+INSERT INTO contato
+(id_ong, nome, email, telefone)
+VALUES(3, 'Cristiano Rodrigues', 'cris.ro7@hotmail.com', '(41)99999-9999');
